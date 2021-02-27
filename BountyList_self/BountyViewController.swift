1de66032a4b418a490069256f820ec14f4318284
LEEYOONJONG <yoonjong1820@khu.ac.kr>
2021-02-27 23:27:39 +0900

    TableView to CollectionView

--- BountyList_self/BountyViewController.swift
+++ BountyList_self/BountyViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class BountyViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class BountyViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     
     let viewModel = BountyViewModel()
      
@@ -21,32 +21,38 @@ class BountyViewController: UIViewController, UITableViewDataSource, UITableView
             }
         }
     }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return viewModel.numOfBountyInfoList
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        // 어떻게 보여줄까?
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? ListCell else{
-            return UITableViewCell()
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "GridCell", for: indexPath) as? GridCell else {
+            return UICollectionViewCell()
         }
-        let bountyInfo = viewModel.bountyInfo(at: indexPath.row)
+        let bountyInfo = viewModel.bountyInfo(at: indexPath.item)
         cell.update(info: bountyInfo)
         return cell
     }
     
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) { // 클릭한다면
-        performSegue(withIdentifier: "showDetail", sender: indexPath.row)
-        print(indexPath.row)
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "showDetail", sender: indexPath.item)
     }
-
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let itemSpacing:CGFloat = 10
+        let textAreaHeight:CGFloat = 65
+        let width:CGFloat = (collectionView.bounds.width - itemSpacing) / 2
+        let height:CGFloat = width*10/7 + textAreaHeight
+        return CGSize(width: width, height: height)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 }
 
-class ListCell:UITableViewCell {
+class GridCell:UICollectionViewCell {
     @IBOutlet weak var imgView:UIImageView!
     @IBOutlet weak var nameLabel:UILabel!
     @IBOutlet weak var bountyLabel:UILabel!
